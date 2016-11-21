8f5aecf989e0a9a87597c054a56855b2c0fa6aac
SandBoat <695514503@qq.com>
2016-11-21 18:02:17 +0800

    添加disabled样式 && 添加错误路径路由

--- http.js
+++ http.js
@@ -1,18 +1,27 @@
+/* 
+本地http服务器搭建步骤
+  1: 安装node
+  2: 控制台进入本文件所在目录
+  3：运行 node http.js
+  4：http://localhost:3000/index.html
+
+  注：一定要在端口号后面加上自己文件路径才能运行成功；出错了请重启服务 （node http.js）
+*/
 var PORT = 3000;
 
 var http = require('http');
-var url=require('url');
-var fs=require('fs');
-var mine=require('./mine').types;
-var path=require('path');
+var url = require('url');
+var fs = require('fs');
+var mine = require('./mine').types;
+var path = require('path');
 
-var server = http.createServer(function (request, response) {
+var server = http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname;
     var realPath = path.join("ef", pathname);
-    //console.log(realPath);
+    // console.log(realPath);
     var ext = path.extname(realPath);
     ext = ext ? ext.slice(1) : 'unknown';
-    fs.exists(realPath, function (exists) {
+    fs.exists(realPath, function(exists) {
         if (!exists) {
             response.writeHead(404, {
                 'Content-Type': 'text/plain'
@@ -21,7 +30,7 @@ var server = http.createServer(function (request, response) {
             response.write("This request URL " + pathname + " was not found on this server.");
             response.end();
         } else {
-            fs.readFile(realPath, "binary", function (err, file) {
+            fs.readFile(realPath, "binary", function(err, file) {
                 if (err) {
                     response.writeHead(500, {
                         'Content-Type': 'text/plain'
